cd1909459bb04c71bb5a240e6c33ae74fd88f8b5
stefanpinter <stefan.pinter@motiondata-vector.com>
2022-06-27 11:42:41 +0200

    helpupdate

--- moi.sh
+++ moi.sh
@@ -50,7 +50,7 @@ showhelp() {
   echo 'Default: "so-you-are-being-scanned"'
   echo "Example: ./$(basename $0) -i \"128.2.1.2|pentesting-scanner-software|1.2.3.4\""
   echo
-  echo "Everything can combined like this:"
+  echo "Everything can be combined like this:"
   echo 'moi -l . -p2 -r -i "192.168.1.1|iamapentestingsoftware"'
 exit 1
 }
